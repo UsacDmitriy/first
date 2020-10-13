00831ed3b5d546c0d3111edba18e910afee362e7
Dmitry Usachev <usach@LAPTOP-D846L8HQ.win.elocont.ru>
2020-10-13 12:58:17 +0400

    classes

---
+++ classes/class_methods.py
@@ -0,0 +1,10 @@
+class Gamer:
+    def def __init__(self, nickname, age, level, points):
+      self.name = name
+      self.age = age
+      self.level = level
+      self.point = points
+
+    def get_nickname(self):
+        return self.nickname
+
